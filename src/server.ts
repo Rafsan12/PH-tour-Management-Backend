ec1287a7f77c5332c320b0d74f7c972b42f5cf8a
Rafsan12 <rafsan.web.2022@gmail.com>
2025-07-08 07:54:17 +0600

    server error habdle

--- src/server.ts
+++ src/server.ts
@@ -19,3 +19,50 @@ const startServer = async () => {
 };
 
 startServer();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal recieved... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("SIGINT", () => {
+  console.log("SIGINT signal recieved... Server shutting down..");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("unhandledRejection", () => {
+  console.log("unhandled Rejection detected  server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+process.on("uncaughtException", () => {
+  console.log("uncaught Exception detected  server shutting down");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    process.exit(1);
+  }
+});
