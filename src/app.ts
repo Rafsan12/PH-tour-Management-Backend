225d680a5dbdf07c220ba3f33fbef77e221c5f63
Rafsan12 <rafsan.web.2022@gmail.com>
2025-07-10 22:08:01 +0600

    not-found route done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
+
 import { globalErrorHandler } from "./app/middlewares/globalError";
+import notFound from "./app/middlewares/NotFound";
 import { router } from "./app/routes";
 
 // middleware
@@ -16,5 +18,8 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to PH Tour management app" });
 });
 
+// Error handle
 app.use(globalErrorHandler);
+app.use(notFound);
+
 export default app;
