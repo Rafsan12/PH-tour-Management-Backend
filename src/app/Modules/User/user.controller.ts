9089e4d1a631cb4dd39e26e3bc4241f9cbfba7ac
Rafsan12 <rafsan.web.2022@gmail.com>
2025-07-11 07:20:51 +0600

    create a higher order function for avoid use trycatch

--- src/app/Modules/User/user.controller.ts
+++ src/app/Modules/User/user.controller.ts
@@ -1,22 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
-import AppError from "../../ErrorHelper/AppError";
 import { userServices } from "./user.service";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    throw new AppError(httpStatus.BAD_REQUEST);
-    const user = await userServices.createUser(req.body);
-    res.status(httpStatus.CREATED).json({
-      message: "User created successfully",
-      user,
+type AsyncHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
+
+const catchAsync =
+  (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((error: any) => {
+      console.log(error), next(error);
     });
-  } catch (error: any) {
-    console.log(error);
-    next(error);
-  }
+  };
+
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  // throw new AppError(httpStatus.BAD_REQUEST);
+  const user = await userServices.createUser(req.body);
+  res.status(httpStatus.CREATED).json({
+    message: "User created successfully",
+    user,
+  });
 };
 
 export const UserController = {
