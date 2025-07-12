36f0d31a0d2efc60cf1f6b736bfc06f93f636160
Rafsan12 <rafsan.web.2022@gmail.com>
2025-07-12 09:42:27 +0600

    create all users api and make a utlis function for send response

--- src/app/Modules/User/user.controller.ts
+++ src/app/Modules/User/user.controller.ts
@@ -1,32 +1,44 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-console */
+
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { userServices } from "./user.service";
 
-type AsyncHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => Promise<void>;
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // throw new AppError(httpStatus.BAD_REQUEST);
+    const user = await userServices.createUser(req.body);
+    // res.status(httpStatus.CREATED).json({
+    //   message: "User created successfully",
+    //   user,
+    // });
 
-const catchAsync =
-  (fn: AsyncHandler) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((error: any) => {
-      console.log(error), next(error);
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User created successfully",
+      data: user,
     });
-  };
+  }
+);
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  // throw new AppError(httpStatus.BAD_REQUEST);
-  const user = await userServices.createUser(req.body);
-  res.status(httpStatus.CREATED).json({
-    message: "User created successfully",
-    user,
-  });
-};
+const getAllUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await userServices.getAllUsers();
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "All User Retrieve successfully",
+      data: result.data,
+      meta: result.meta,
+    });
+  }
+);
 
 export const UserController = {
   createUser,
+  getAllUser,
 };
